90b08d1380de4c80a893ffe151cadc5aa088b6c3
VARUN JAIN <varun13@gmail.com>
2015-11-18 15:33:40 +0530

    removed aws_access_key from deploy attribute

--- ha_certs/recipes/cert_install.rb
+++ ha_certs/recipes/cert_install.rb
@@ -17,8 +17,8 @@ aws_s3_file File.join(node["cert_install"]['local_dir'], node["cert_install"]['r
 	remote_path node["cert_install"]['remote_path']
 	#s3_url node["cert_install"]['s3_url']
 	# cert_install is the name of the app to setup env variables for AWS keys for security
-	aws_access_key_id node[:deploy]['cert_install'][:environment_variables]['aws_access_key_id']
-	aws_secret_access_key node[:deploy]['cert_install'][:environment_variables]['aws_secret_access_key']
+	#aws_access_key_id node[:deploy]['cert_install'][:environment_variables]['aws_access_key_id']
+	#aws_secret_access_key node[:deploy]['cert_install'][:environment_variables]['aws_secret_access_key']
 	owner 'root'
 	group 'root'
 	mode '0755'
