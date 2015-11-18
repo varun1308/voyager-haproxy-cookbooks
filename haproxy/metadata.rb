0053c755be2104b65ecb1c482b0226cfd1497dad
VARUN JAIN <varun13@gmail.com>
2015-11-18 23:11:46 +0530

    added haproxy config template cookbook

---
+++ haproxy/metadata.rb
@@ -0,0 +1,7 @@
+name 'haproxy'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures haproxy'
+long_description 'Installs/Configures haproxy'
+version '0.1.0'
